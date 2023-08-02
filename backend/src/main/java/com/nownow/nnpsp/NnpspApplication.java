5c82520d07fb7ac35d62fb8c63c1a62e6caaa13e
DanielAdek <maildaniel.me1@gmail.com>
2023-08-02 17:32:06 +0100

    rename file

---
+++ backend/src/main/java/com/nownow/nnpsp/NnpspApplication.java
@@ -0,0 +1,13 @@
+package com.nownow.nnpsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpspApplication.class, args);
+	}
+
+}
