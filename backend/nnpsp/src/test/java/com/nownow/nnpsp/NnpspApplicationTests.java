08c01614c12c458c43bfc0c18fea904d9e133299
DanielAdek <maildaniel.me1@gmail.com>
2023-08-02 17:29:29 +0100

    project scafold

---
+++ backend/nnpsp/src/test/java/com/nownow/nnpsp/NnpspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nownow.nnpsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
